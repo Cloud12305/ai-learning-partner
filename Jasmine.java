b8d5f5d7b5256ca05da02897273791eb9b0c19aa
Jasmine <15268660761@163.com>
2025-08-09 01:35:01 +0800

    Jasmine Branch

--- Jasmine.java
+++ Jasmine.java
@@ -1,6 +1,5 @@
-public class JasmineBranch {
+public class Jasmine {
     public static void main(String[] args) {
-        System.out.println("这是Jasmine的Branch");
         System.out.println("My name is Jasmine");
     }
 }
