4e6df257882845abe1b9648a43c30d1a0b53b95c
Jasmine <15268660761@163.com>
2025-11-10 01:32:48 +0800

    加了EmotionRecordRepository

--- learn-buddy-backend/src/main/java/com/digitalhorsepower/learnbuddy/entity/EmotionRecord.java
+++ learn-buddy-backend/src/main/java/com/digitalhorsepower/learnbuddy/entity/EmotionRecord.java
@@ -1,4 +1,20 @@
 package com.digitalhorsepower.learnbuddy.entity;
 
+import javax.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "emotion_record")
 public class EmotionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;
+    private String text;
+    private String emotion;
+    private String createdAt;
 }
